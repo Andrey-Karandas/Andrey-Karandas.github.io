ec6977b9512baa3118db012d04aa314f7f45e58e
Andrey <70054885+Andrey-Karandas@users.noreply.github.com>
2024-03-08 18:18:38 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const select = document.querySelector('.select')
-document.querySelector('.btn').addEventListener('click', () => {
-  const date = new Date().toLocaleDateString()
-  const url = `https://api.telegram.org/bot6333643693:AAFxgvOteH9N2-WkapNWvWXlx5c3EK0ZLLs/sendMessage?chat_id=756864009&text=${select.value} навбрав воду ${date}`
-  let xhttp = new XMLHttpRequest()
-  xhttp.open('GET', url, true)
-  xhttp.send()
-})
